68084e3738d1e63f33fa222b8744ed0a57a9805d
i80287 <addmlbx@gmail.com>
2024-09-23 00:52:04 +0300

    changes

--- run_gcc_tests.sh
+++ run_gcc_tests.sh
@@ -4,10 +4,7 @@ build_dir=cmake-build-tests-gcc
 
 set -e
 
-if [ -d "$build_dir" ]; then rm -r "$build_dir"; fi
 mkdir -p "$build_dir"
-cp ./u64-primes.txt ./$build_dir/u64-primes.txt
-cp ./u128-primes.txt ./$build_dir/u128-primes.txt
 cd ./$build_dir || return 1
 
 cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S .. -B . &&
