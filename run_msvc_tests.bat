5e04b20fb2f68e0856c973550d029dbd8f461870
i80287 <addmlbx@gmail.com>
2024-10-04 19:00:26 +0300

    changes

--- run_msvc_tests.bat
+++ run_msvc_tests.bat
@@ -2,5 +2,6 @@ if not exist ".\cmake-build-tests-windows-msvc" mkdir cmake-build-tests-windows-
 cd .\cmake-build-tests-windows-msvc
 
 cmake -G "Visual Studio 17 2022" -A x64 -S .. -B .
-dir
-msbuild tests.sln -t:rebuild -verbosity:diag -property:Configuration=Release
+@REM dir
+@REM -verbosity:diag
+msbuild tests.sln -t:rebuild -property:Configuration=Release
