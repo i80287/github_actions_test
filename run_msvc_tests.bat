35f6fa8239c3ca867d17dedb262ba707af4b2d91
i80287 <addmlbx@gmail.com>
2024-10-04 19:11:20 +0300

    changes

--- run_msvc_tests.bat
+++ run_msvc_tests.bat
@@ -4,4 +4,4 @@ cd .\cmake-build-tests-windows-msvc
 cmake -G "Visual Studio 17 2022" -A x64 -S .. -B .
 @REM dir
 @REM -verbosity:diag
-msbuild tests.sln -t:rebuild -property:Configuration=Release
+msbuild tests.sln -t:rebuild -property:Configuration=Release -noWarn:C4514;C4710
